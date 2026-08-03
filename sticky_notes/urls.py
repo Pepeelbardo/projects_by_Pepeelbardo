0a186e429f58207b73a18269564ee35f9db2d141
Pepeelbardo <jlsarabian@oulook.com>
2026-08-03 18:30:53 -0500

    Add sticky notes app with unit tests

---
+++ sticky_notes/urls.py
@@ -0,0 +1,9 @@
+"""Root URL configuration for the sticky_notes project."""
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("notes.urls")),
+]
